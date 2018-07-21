84dbf2f77778ef1d9628ef7a0ee6d369220fc2b7
Zain's University Laptop <zain.zain51@gmail.com>
2018-07-21 17:51:40 +1200

    Explaining branching to a team mate (demo commit)

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,6 +2,6 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The project imports, builds and runs successfully!");
+        System.out.println("Test commit");
     }
 }
