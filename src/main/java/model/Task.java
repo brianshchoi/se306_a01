8be4a93ed8031fc46a619d7a66e66e814702ac38
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2018-07-25 10:47:42 +1200

    Constructed Task class which represents node in task graph.  Task also stores costs between tasks.

---
+++ src/main/java/model/Task.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Task {
+    private Map<Task, Integer> parents = new HashMap<>();
+    private Map<Task, Integer> children = new HashMap<>();
+
+    private int time;
+    private String name;
+
+    public Task(String name, int time) {
+        this.name = name;
+        this.time = time;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void insertLinkToParent(Task parent, int cost) {
+        this.parents.put(parent, cost);
+    }
+
+    public void insertLinkToChild(Task child, int cost) {
+        this.children.put(child, cost);
+    }
+
+    public Set<Task> getChildren() {
+        return this.children.keySet();
+    }
+
+    public Set<Task> getParents() {
+        return this.parents.keySet();
+    }
+
+    public int getParentLinkCost(Task parent) {
+        return parents.get(parent);
+    }
+
+    public int getChildLinkCost(Task child) {
+        return children.get(child);
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
