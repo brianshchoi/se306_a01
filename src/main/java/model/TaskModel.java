517450cebb7d1e8911c254456f93d1b2ff8503d7
Zain's University Laptop <zain.zain51@gmail.com>
2018-07-25 15:52:09 +1200

    Figured out how to add dependencies and keep track of entry nodes

--- src/main/java/model/TaskModel.java
+++ src/main/java/model/TaskModel.java
@@ -6,6 +6,7 @@ import java.util.Set;
 public class TaskModel {
     private TaskModel instance = null;
     private Set<Task> tasks = new HashSet<>();
+    private Set<Task> entryTasks = new HashSet<>();
 
     private TaskModel() {}
 
@@ -19,5 +20,16 @@ public class TaskModel {
 
     public void addTask(Task task) {
         tasks.add(task);
+        entryTasks.add(task);
+    }
+
+    public void addDependency(Task parent, Task child, int cost) {
+        parent.insertLinkToChild(child, cost);
+        child.insertLinkToParent(parent, cost);
+        entryTasks.remove(child);
+    }
+
+    public Set<Task> getEntryTasks() {
+        return entryTasks;
     }
 }
