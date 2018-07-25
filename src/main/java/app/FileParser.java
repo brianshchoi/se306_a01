7489e7fe905e4b656bea2a3216b9ad01f7f179ac
Zain's University Laptop <zain.zain51@gmail.com>
2018-07-25 20:07:56 +1200

    Added FileParser skeleton class

---
+++ src/main/java/app/FileParser.java
@@ -0,0 +1,24 @@
+package app;
+
+import model.TaskModel;
+
+import java.io.File;
+
+public class FileParser {
+    private File file;
+
+    public FileParser(String filename) {
+        //TODO: create file object
+    }
+
+    public TaskModel getTaskModelFromFile() {
+        //TODO:  get TaskModelInstance
+        /* TODO:
+        * Read through file
+        * Filter out dependencies
+        * Create Task objects and add to the TaskModel
+        * Then iterate through the the dependencies and create them by invoking
+         * addDependency() on the TaskModel object*/
+        return null;
+    }
+}
