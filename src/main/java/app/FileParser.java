7eb9ec7c33d5a51d9dec7e9acdcddc495793b01b
Zain's University Laptop <zain.zain51@gmail.com>
2018-07-25 23:04:53 +1200

    Added regex helper to FileParser

--- src/main/java/app/FileParser.java
+++ src/main/java/app/FileParser.java
@@ -3,6 +3,10 @@ package app;
 import model.TaskModel;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileParser {
     private File file;
@@ -20,5 +24,27 @@ public class FileParser {
         * Then iterate through the the dependencies and create them by invoking
          * addDependency() on the TaskModel object*/
         return null;
+    }
+
+    /**
+     * Use this method to take a given line in a file and extract the important deatils.
+     * e.g.
+     * Passing "    a   [Weight=2];" will return a List<String> containing "a" and "2"
+     * Passing "    a -> b   [Weight=1];" will return a List<String> containing "a", "b", and 1
+     * @param line
+     * @return
+     */
+    private List<String> extractDetails(String line) {
+        line.trim();
+        System.out.println(line.contains("->"));
+        line = line.trim();
+        Pattern pattern = Pattern.compile("\\w\\s+|\\d");
+        Matcher matcher = pattern.matcher(line);
+        List<String> out = new ArrayList<>();
+        while (matcher.find()) {
+            out.add(matcher.group());
+        }
+
+        return out;
     }
 }
