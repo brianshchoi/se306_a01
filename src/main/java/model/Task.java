8e4de160009d100a8b706e849c1375aeb195142b
Woojin Kang <wkang9497@gmail.com>
2018-07-25 21:26:32 +1200

    Started documentation on TaskModel

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -16,26 +16,52 @@ public class Task {
         this.weight = weight;
     }
 
+    /**
+     * Returns the Weight of the Task as int
+     * @return int weight
+     */
     public int getWeight() {
         return weight;
     }
 
+    /**
+     * Returns the name of the task
+     * @return String name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Maps parent task and cost to the parent together
+     * @param parent
+     * @param cost
+     */
     public void insertLinkToParent(Task parent, int cost) {
         this.parents.put(parent, cost);
     }
 
+    /**
+     * Maps child task and cost to the child together
+     * @param child
+     * @param cost
+     */
     public void insertLinkToChild(Task child, int cost) {
         this.children.put(child, cost);
     }
 
+    /**
+     * Returns a set of children tasks
+     * @return Set<Task>
+     */
     public Set<Task> getChildren() {
         return this.children.keySet();
     }
 
+    /**
+     * Returns a set of parent tasks
+     * @return Set<Task>
+     */
     public Set<Task> getParents() {
         return this.parents.keySet();
     }
