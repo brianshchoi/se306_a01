38fc3e3aa4191541145105b1b18d999afe0470ee
Sze Meeng Tan <stan557@aucklanduni.ac.nz>
2018-07-26 17:54:33 +1200

    added commons cli library and completed some of zain's todos

--- src/main/java/app/CLI.java
+++ src/main/java/app/CLI.java
@@ -1,9 +1,11 @@
 package app;
 
+import org.apache.commons.cli.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 
 /**
  * This class represents the Command Line Interface (CLI) of the application.
@@ -26,18 +28,28 @@ public class CLI {
     private static int algorithmCores = 1;
 
     public static void main(String[] args) {
-        // TODO: 25/07/2018 convert args array to List<String> object using Arrays.asList 
+        // TODO: 25/07/2018 convert args array to List<String> object using Arrays.asList
+        List<String> argsList = Arrays.asList(args);
 
-        // TODO: 25/07/2018 extract filename from the list object (first item) 
+
+        // TODO: 25/07/2018 extract filename from the list object (first item)
+        outputFilename = argsList.get(0);
 
         // TODO: 25/07/2018 extract number of processors from the list object (second item)
+        algorithmCores = Integer.parseInt(argsList.get(1));
 
         // TODO: 25/07/2018 check if list contains "-v" flag and set boolean field accordingly
+        if (argsList.contains("-v")){
+            visualisation = true;
+        }
 
         // TODO: 25/07/2018 check if list contains "-p" and if so, get the index of this String, add 1 to it, and lookup N using the resulting index
-
+        if (argsList.contains("-p")){
+            int index = argsList.indexOf("p");
+            algorithmCores = index +1;
+        }
         // TODO: 25/07/2018 set algorithmCores appropriately if you need to
-        
+
         // TODO: 25/07/2018 check if list contains "-o" and if so, do same thing as above (lookup indexOf("-o")+1 to get OUTPUT)
 
         // TODO: 25/07/2018 set outputFilename accordingly if you need to
@@ -45,5 +57,30 @@ public class CLI {
         // TODO: 25/07/2018 instantiate FileParser and pass filename into constructor.
 
         // TODO: 25/07/2018 invoke appropriate method on FileParser object to get TaskModel object.  Store this in local variable
-    }
+
+//        Options options = new Options();
+//
+//
+//
+//        Option input = Option.builder("INPUT").required(true).argName("INPUT.dot").build();
+//        input.setRequired(true);
+//        options.addOption(input);
+//
+//        CommandLineParser parser = new DefaultParser();
+//        HelpFormatter formatter = new HelpFormatter();
+//        CommandLine cmd;
+//
+//        try{
+//            cmd = parser.parse(options, args);
+//
+//            String inputFilePath = cmd.getOptionValue("input");
+//            System.out.println(inputFilePath);
+//        } catch (ParseException e) {
+//            System.out.println(e.getMessage());
+//            formatter.printHelp("java -jar scheduler.jar INPUT.dot P [OPTION]", options);
+//
+//            System.exit(1);
+//        }
+//
+//    }
 }
