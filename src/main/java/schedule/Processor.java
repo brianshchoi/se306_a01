5f30d0d7367fea4baf23c6625ef6815d1299e8cc
Zain's University Laptop <zain.zain51@gmail.com>
2018-07-26 17:11:10 +1200

    Realised there's a problem with interface - add method in Processor does not receive Task object

--- src/main/java/schedule/Processor.java
+++ src/main/java/schedule/Processor.java
@@ -2,7 +2,11 @@ package schedule;
 
 import model.Task;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Processor {
+    private Map<Integer, Task> timeTaskMap = new HashMap<>();
 
     public int addAtTime(int time) {
         return 0;
