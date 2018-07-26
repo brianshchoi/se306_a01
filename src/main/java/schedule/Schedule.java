a63c019681d3e8531a64ebc266451acdd4c513a9
Woojin Kang <wkang9497@gmail.com>
2018-07-26 17:47:09 +1200

    Added todos

--- src/main/java/schedule/Schedule.java
+++ src/main/java/schedule/Schedule.java
@@ -2,13 +2,13 @@ package schedule;
 
 import model.Task;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Schedule {
 
-    Map<Integer, Processor> processors;
+    Map<Integer, Processor> processors = new HashMap<>();
 
-    Map<>
 
     public int getMakeSpan() {
 
@@ -16,8 +16,12 @@ public class Schedule {
         return 0;
     }
 
-    public void scheduleOnProcessor(int processor, Task task) {
+    public void scheduleOnProcessor(int processor, Task task, int scheduleTime) {
+        // TODO: 26/07/2018 Get the processor from the map by looking up using the processor num
 
+        // TODO: 26/07/2018 invoke a method on the processor object we received that lets us schedule a task
+
+        // TODO: 26/07/2018 pass in the task to be scheduled, and the time to schedule it at
     }
 
 
