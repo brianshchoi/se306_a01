453160d0b9d21bc3a78d8500b4996b91bb075eb7
Woojin Kang <wkang9497@gmail.com>
2018-07-26 17:57:14 +1200

    implemented scheduleOnProcessor for Schedule Class

--- src/main/java/schedule/Schedule.java
+++ src/main/java/schedule/Schedule.java
@@ -17,11 +17,10 @@ public class Schedule {
     }
 
     public void scheduleOnProcessor(int processor, Task task, int scheduleTime) {
-        // TODO: 26/07/2018 Get the processor from the map by looking up using the processor num
 
-        // TODO: 26/07/2018 invoke a method on the processor object we received that lets us schedule a task
 
-        // TODO: 26/07/2018 pass in the task to be scheduled, and the time to schedule it at
+        Processor proc = processors.get(processor);
+        proc.addAtTime(scheduleTime, task);
     }
 
 
