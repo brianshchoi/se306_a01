9aee701a8cf23ffe28f192501b8657adb1d01c62
Zain's University Laptop <zain.zain51@gmail.com>
2018-07-29 16:22:12 +1200

    starting fresh in FileParser

--- src/main/java/app/FileParser.java
+++ src/main/java/app/FileParser.java
@@ -13,7 +13,6 @@ public class FileParser {
     private File file;
     private FileReader fr;
 
-    // Input absolute path for filename
     public FileParser(String filename) {
         this.file = new File(filename);
         try {
@@ -23,74 +22,7 @@ public class FileParser {
         }
     }
 
-//    public TaskModel getTaskModelFromFile() throws IOException {
     public TaskModel getTaskModelFromFile() {
-        BufferedReader br = new BufferedReader(fr);
-        try {
-            String currentLine;
-
-            // While there is another line, do shit
-            while ((currentLine = br.readLine()) != null) {
-                List<String> extract = extractDetails(currentLine);
-
-                for (String anExtract : extract) {
-                    System.out.println(anExtract);
-                }
-                System.out.println(currentLine);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                br.close();
-                if (fr != null)
-                    fr.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-
-        //TODO:  get TaskModelInstance
-        /* TODO:
-        * Read through file
-        * Filter out dependencies
-        * Create Task objects and add to the TaskModel
-        * Then iterate through the the dependencies and create them by invoking
-         * addDependency() on the TaskModel object*/
-//        return null;
         return null;
-    }
-
-    /**
-     * Use this method to take a given line in a file and extract the important details.
-     * e.g.
-     * Passing "    a   [Weight=2];" will return a List<String> containing "a" and "2"
-     * Passing "    a -> b   [Weight=1];" will return a List<String> containing "a", "b", and 1
-     * @param line
-     * @return
-     */
-    private List<String> extractDetails(String line) {
-        // Check if line is a dependency
-        if (line.contains("->")){
-
-
-        }
-
-        System.out.println(line.contains("->"));
-        line = line.trim();
-        Pattern pattern = Pattern.compile("\\w\\s+|\\d+");
-        Matcher matcher = pattern.matcher(line);
-        List<String> out = new ArrayList<>();
-        while (matcher.find()) {
-            out.add(matcher.group());
-        }
-
-        return out;
-    }
-
-    // Returns the absolute pathname string of this abstract pathname.
-    public String getAbsolutePath() {
-        return file.getAbsolutePath();
     }
 }
