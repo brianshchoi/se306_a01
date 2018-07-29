d5e7176e80bd1866316e3d2f051f79e2ff60504d
Zain Azimullah (Linux) <zain.zain51@gmail.com>
2018-07-30 01:07:56 +1200

    Working on fileParser

--- src/main/java/app/FileParser.java
+++ src/main/java/app/FileParser.java
@@ -1,5 +1,8 @@
 package app;
 
+import com.paypal.digraph.parser.GraphNode;
+import com.paypal.digraph.parser.GraphParser;
+import model.Task;
 import model.TaskModel;
 
 import java.io.File;
@@ -8,12 +11,19 @@ import java.io.FileNotFoundException;
 
 public class FileParser {
     private FileInputStream fileInputStream;
+    private TaskModel taskModel = new TaskModel();
 
     public FileParser(File file) throws FileNotFoundException {
         fileInputStream = new FileInputStream(file);
     }
 
     public TaskModel getTaskModelFromFile() {
-        return null;
+        GraphParser parser = new GraphParser(fileInputStream);
+
+        for (GraphNode node: parser.getNodes().values()) {
+            taskModel.addTask(new Task(node.getId(), (Integer) node.getAttributes().get("Weight")));
+        }
+
+        return taskModel;
     }
 }
