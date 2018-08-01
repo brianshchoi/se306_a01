b0bc1db9130ee12cef5a41873b7f810429945a65
Zain's University Laptop <zain.zain51@gmail.com>
2018-08-01 14:26:21 +1200

    Created empty unit test for scheduler

---
+++ src/test/java/scheduleModel/SchedulerTest.java
@@ -0,0 +1,16 @@
+package scheduleModel;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class SchedulerTest {
+
+    private IScheduler scheduler;
+
+    @Before
+    public void setup() {
+
+    }
+
+}
