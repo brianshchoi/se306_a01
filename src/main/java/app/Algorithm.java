522016c20d5b7f36659de9d50661032e9decfa7f
lucyJiang279 <ljia374@aucklanduni.ac.nz>
2018-08-01 15:02:53 +1200

    started working on algorithm

--- src/main/java/app/Algorithm.java
+++ src/main/java/app/Algorithm.java
@@ -1,19 +1,56 @@
 package app;
 
+import scheduleModel.IProcessor;
 import scheduleModel.ISchedule;
+import scheduleModel.IScheduler;
+import taskModel.Task;
 import taskModel.TaskModel;
 
-public class Algorithm {
-    
-    // TODO: 31/07/2018 Create fields here
-    private TaskModel taskModel;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Algorithm(TaskModel taskModel) {
+public class Algorithm {
+
+    private TaskModel taskModel;
+    private IScheduler scheduler;
+
+    public Algorithm(TaskModel taskModel, IScheduler scheduler) {
         this.taskModel = taskModel;
+        this.scheduler = scheduler;
     }
 
-    public ISchedule run() {
-        // TODO: 31/07/2018 Implement algorithm
-        throw new UnimplmentedException();
+    public ISchedule run(Task cTask, IProcessor cProc, Task pTask, IProcessor pProc, List<Task> freeTasks, int depth, ISchedule schedule, ISchedule bestSchedule, int bound) {
+        if (!freeTasks.isEmpty()) {
+            for (int i = 0; i < freeTasks.size(); i++){
+                for (int j = 0; j < schedule.getProcessors().size(); j++) {
+                    pTask = cTask;
+                    pProc = cProc;
+                    cTask = freeTasks.get(i);
+                    cProc = schedule.getProcessors().get(j);
+
+                    if (cTask.equals(pTask) || (j == 0 && pProc.equals(schedule.getProcessors().get(schedule.getProcessors().size() - 1)))){
+                        scheduler.remove(pTask);
+                    }
+
+                    scheduler.schedule(cTask, cProc);
+                    depth++;
+
+                    List<Task> newFreeTasks = new ArrayList<>();
+                    //somehow update newFreeTasks
+
+                    if (schedule.getFinishTime() < bound) {
+                        int numTasks = taskModel.getTasks().size();
+                        if (depth == numTasks) {
+                            bestSchedule = schedule;
+                            bound = bestSchedule.getFinishTime();
+                        } else if (depth < numTasks) {
+                            bestSchedule = run(cTask, cProc, pTask, pProc, newFreeTasks, depth, schedule, bestSchedule, bound);
+                        }
+                    }
+                    depth--;
+                }
+            }
+        }
+        return bestSchedule;
     }
 }
