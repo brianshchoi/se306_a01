7c4ef2207d27b4494f4828ac520bd34c155c15e2
Brian <sukhoonchoi1@gmail.com>
2018-08-03 16:18:18 +1200

    Replace StringBuilder with just String

--- src/main/java/app/DotRenderer.java
+++ src/main/java/app/DotRenderer.java
@@ -13,52 +13,40 @@ public class DotRenderer {
     }
 
     public static String openGraph(String title) {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("digraph \"");
-        sb.append(title);
-        sb.append("\" {");
-        sb.append(NEWLINE);
-
-        return sb.toString();
+        return "digraph \"" +
+                title +
+                "\" {" +
+                NEWLINE;
     }
 
     public static String addNode(Task task, int startTime, IProcessor processor){
-        final StringBuilder sb = new StringBuilder();
-        sb.append(TAB);
-        sb.append(task.getName());
-        sb.append(TAB);
-        sb.append("[Weight=");
-        sb.append(task.getWeight());
-        sb.append(",Start=");
-        sb.append(startTime);
-        sb.append(",Processor=");
-        sb.append(processor.toString());
-        sb.append("];");
-        sb.append(NEWLINE);
-
-        return sb.toString();
+        return TAB +
+                task.getName() +
+                TAB +
+                "[Weight=" +
+                task.getWeight() +
+                ",Start=" +
+                startTime +
+                ",Processor=" +
+                processor.toString() +
+                "];" +
+                NEWLINE;
     }
 
     public static String addDependency(Task parent, Task child){
-        final StringBuilder sb = new StringBuilder();
-        sb.append(TAB);
-        sb.append(parent.getName());
-        sb.append(" -> ");
-        sb.append(child.getName());
-        sb.append(TAB);
-        sb.append("[Weight=");
-        sb.append(parent.getChildLinkCost(child));
-        sb.append("];");
-        sb.append(NEWLINE);
-
-        return sb.toString();
+        return TAB +
+                parent.getName() +
+                " -> " +
+                child.getName() +
+                TAB +
+                "[Weight=" +
+                parent.getChildLinkCost(child) +
+                "];" +
+                NEWLINE;
     }
 
 
     public static String closeGraph(){
-        final StringBuilder sb = new StringBuilder();
-        sb.append("}");
-
-        return sb.toString();
+        return "}";
     }
 }
