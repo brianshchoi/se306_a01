49c2a6fc62b27d99d9e878a09804f024b9e820eb
Zain's University Laptop <zain.zain51@gmail.com>
2018-08-04 17:28:27 +1200

    refactoring

--- src/main/java/app/exception/InvalidScheduleException.java
+++ src/main/java/app/exception/InvalidScheduleException.java
@@ -1,4 +1,4 @@
-package app;
+package app.exception;
 
 public class InvalidScheduleException extends RuntimeException {
     public InvalidScheduleException(String message) {
--- src/main/java/app/exception/UnimplmentedException.java
+++ src/main/java/app/exception/UnimplmentedException.java
@@ -1,4 +1,4 @@
-package app;
+package app.exception;
 
 public class UnimplmentedException extends RuntimeException {
 }
