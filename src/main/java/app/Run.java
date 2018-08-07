c2fef5e434ff18ecf7fac5e38cecb0d22aca1978
Zain's University Laptop <zain.zain51@gmail.com>
2018-08-07 16:47:20 +1200

    Commented best known run time

--- src/main/java/app/Run.java
+++ src/main/java/app/Run.java
@@ -11,10 +11,10 @@ public class Run {
     private static final String NODES_8 = "Nodes_8_Random.dot";
     private static final String NODES_9 = "Nodes_9_SeriesParallel.dot";
     private static final String NODES_10 = "Nodes_10_Random.dot";
-    private static final String NODES_11 = "Nodes_11_OutTree.dot";
+    private static final String NODES_11 = "Nodes_11_OutTree.dot"; // 32 seconds
 
     private static final String FILENAME = NODES_7; // SET INPUT HERE
-    private static final int NUM_PROCESSORS = 2;
+    private static final int NUM_PROCESSORS = 2; // SET PROCESSORS HERE
 
     public  static void main(String[] args) throws FileNotFoundException, URISyntaxException, CloneNotSupportedException {
         // Start timing algorithm
