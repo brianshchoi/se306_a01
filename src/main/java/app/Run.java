64d2f09c11815d02781b8c7c4beea677d7ff75a7
Zain's University Laptop <zain.zain51@gmail.com>
2018-08-07 15:40:57 +1200

    Implemented timer in Run

--- src/main/java/app/Run.java
+++ src/main/java/app/Run.java
@@ -13,10 +13,18 @@ public class Run {
     private static final String NODES_10 = "Nodes_10_Random.dot";
     private static final String NODES_11 = "Nodes_11_OutTree.dot";
 
-    private static final String FILENAME = NODES_7; // SET INPUT HERE
+    private static final String FILENAME = NODES_11; // SET INPUT HERE
     private static final int NUM_PROCESSORS = 2;
 
     public  static void main(String[] args) throws FileNotFoundException, URISyntaxException, CloneNotSupportedException {
+        // Start timing algorithm
+        final double start = System.currentTimeMillis();
+
+        // Run algorithm
         new DFSAlgorithm(new FileParser(new File(Run.class.getResource(FILENAME).toURI())).getTaskModelFromFile(), NUM_PROCESSORS).run().debug();
+
+        // Stop timing algorithm
+        final double end = System.currentTimeMillis();
+        System.out.println("Total execution time: " + (end - start)/1000 + " milliseconds");
     }
 }
