23c7d1972a13ee868e7b1142ac3fbbe87fb31aa6
Brian <sukhoonchoi1@gmail.com>
2018-08-08 22:07:20 +1200

    Fix tilesfx gradle dependency and implement basic tile

--- src/main/java/view/Visualizer.java
+++ src/main/java/view/Visualizer.java
@@ -1,19 +1,46 @@
 package view;
 
+import eu.hansolo.tilesfx.Tile;
+import eu.hansolo.tilesfx.TileBuilder;
+import eu.hansolo.tilesfx.tools.FlowGridPane;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+
 public class Visualizer extends Application {
+    private static final double TILE_SIZE = 150;
+
+
+    private Tile tile;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(this.getClass().getResource("Visualizer.fxml"));
-        Parent layout = loader.load();
+        tile = TileBuilder.create().prefSize(TILE_SIZE, TILE_SIZE)
+                .skinType(Tile.SkinType.CLOCK)
+                .title("Clock Tile")
+                .text("Whatever text")
+                .dateVisible(true)
+                .locale(Locale.US)
+                .running(true)
+                .build();
 
-        Scene scene = new Scene(layout);
+        FlowGridPane pane = new FlowGridPane(5,5, tile);
+        pane.setHgap(5);
+        pane.setVgap(5);
+        pane.setPadding(new Insets(5));
+        pane.setBackground(new Background(new BackgroundFill(Tile.BACKGROUND.darker(), CornerRadii.EMPTY, Insets.EMPTY)));
+
+        Scene scene = new Scene(pane);
+
+        primaryStage.setTitle("TilesFX Dashboard using Medusa");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
