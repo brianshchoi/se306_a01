5072f95142d0b42f9b2b6680a8f21d3550e6623e
Zain's University Laptop <zain.zain51@gmail.com>
2018-08-12 02:24:07 +1200

    IMPORTANT FIX DONE - Fixed scheduling bug - was not considering free time between tasks

--- src/main/java/scheduleModel/Processor.java
+++ src/main/java/scheduleModel/Processor.java
@@ -39,10 +39,13 @@ public class Processor implements IProcessor, Cloneable {
 
     @Override
     public int getFinishTime() {
-        if (tasks.size() == 0) return 0;
+        int finishTime = 0;
+        for (Map.Entry<Task, Integer> entry: taskMap.entrySet()) {
+            int taskFinishTime = entry.getValue() + entry.getKey().getWeight();
+            if (taskFinishTime > finishTime) finishTime = taskFinishTime;
+        }
 
-        Task latestTaskScheduled = tasks.get(tasks.size() - 1);
-        return taskMap.get(latestTaskScheduled) + latestTaskScheduled.getWeight();
+        return finishTime;
     }
 
     @Override
