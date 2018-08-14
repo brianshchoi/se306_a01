acd00b5b8e99a882028f5eafa43a1c88f4dea00b
Zain's University Laptop <zain.zain51@gmail.com>
2018-08-14 13:49:25 +1200

    tidyup

--- src/main/java/app/DFSAlgorithm.java
+++ src/main/java/app/DFSAlgorithm.java
@@ -36,20 +36,27 @@ public class DFSAlgorithm implements IAlgorithm {
         return bestSchedule;
     }
 
-    private void run(List<Task> freeTasks, int depth, ISchedule schedule, Set<Task> cleanPTasks, IProcessor pProc) {
+    private void run(List<Task> freeTasks, int depth, ISchedule schedule, Set<Task> cleanPreviousTasks, IProcessor previousProcessor) {
         recursionLevel++;
 
         if (!freeTasks.isEmpty()) {
-            Set<Task> pTasks = new HashSet<>();
-            pTasks.addAll(cleanPTasks);
+            // Get the previous tasks above this layer in the recursion call
+            Set<Task> previousTasks = new HashSet<>(cleanPreviousTasks);
+
+            // Iterate through each eligible task
             for (Task currentTask : freeTasks) {
 
+                // If the task we are currently scheduling
+                // has been already scheduled in the past after scheduling
+                // some other task in this layer, then this task has already been
+                // tried on all the processors.  There is no point trying this task
+                // on all the processors again - we should just stick them on the same
+                // processor as that is the only thing that can give us something different.
                 Set<IProcessor> processors = new HashSet<>();
-                if (pTasks.contains(currentTask)) {
-                    processors.add(pProc);
-                    //System.out.println("true");
+                if (previousTasks.contains(currentTask)) {
+                    processors.add(previousProcessor);
                 } else {
-                    processors.addAll(schedule.getProcessors());
+                    processors.addAll(schedule.getProcessors()); // Otherwise we need to try it on all processors
                 }
 
                 // Try scheduling task on each processor and add copy to set of unique schedules
@@ -64,25 +71,17 @@ public class DFSAlgorithm implements IAlgorithm {
                     scheduler.remove(currentTask, schedule);
                 }
 
-                pTasks.add(currentTask);
+                // Remember that we have scheduled this task.
+                // When we come back from the recursive call and go onto the
+                // next currentTask, we know from previousTasks that THIS currentTask has been
+                // tried on all the processors.
+                previousTasks.add(currentTask);
 
                 // Go through each of the unique created schedules at this level
                 for (ISchedule currentSchedule : schedules) {
-//                    List<Integer> finishTimes = new ArrayList<>();
-//                    for (IProcessor processor: currentSchedule.getProcessors()){
-//                        finishTimes.add(processor.getFinishTime());
-//                    }
-
-
-
-
-
-
                     numBranches++;
                     depth++;
 
-
-                    IProcessor newPProc = currentSchedule.getProcessorOf(currentTask);
                     // Check if bad schedule
                     if (currentSchedule.getFinishTime() < bound) {
                         int numTasks = taskModel.getTasks().size();
@@ -97,9 +96,7 @@ public class DFSAlgorithm implements IAlgorithm {
                             // Set new list of free tasks
                             List<Task> newFreeTasks = getFreeTasks(currentSchedule, taskModel.getTasks());
                             if (cost(currentSchedule) < bound) {
-
-                                run(newFreeTasks, depth, currentSchedule, pTasks, newPProc);
-
+                                run(newFreeTasks, depth, currentSchedule, previousTasks, currentSchedule.getProcessorOf(currentTask));
                             }
                         }
                     }
