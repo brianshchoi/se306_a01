02a398a79c88f43ba66522c31738743cc0977e39
Zain's University Laptop <zain.zain51@gmail.com>
2018-08-17 20:02:03 +1200

    commented task classes

--- src/main/java/scheduleModel/IncorrectArgumentsException.java
+++ src/main/java/scheduleModel/IncorrectArgumentsException.java
@@ -4,7 +4,6 @@ package scheduleModel;
  * This exception is thrown when the user does not enter a valid number of arguments.
  */
 public class IncorrectArgumentsException extends RuntimeException {
-
     public IncorrectArgumentsException(String string) {
         super(string);
     }
--- src/main/java/taskModel/TaskNotFoundException.java
+++ src/main/java/taskModel/TaskNotFoundException.java
@@ -1,4 +1,8 @@
 package taskModel;
 
+/**
+ * Throw this when a Task cannot be found in an aggregate object
+ * like TaskModel, Schedule, etc.
+ */
 public class TaskNotFoundException extends RuntimeException {
 }
