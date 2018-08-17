a415b1e9fb045b2677e7a67e056d39912ff999d8
Sze Meeng Tan <stan557@aucklanduni.ac.nz>
2018-08-17 19:05:16 +1200

    Comments in AlgorithmListener

--- src/main/java/view/listeners/AlgorithmListener.java
+++ src/main/java/view/listeners/AlgorithmListener.java
@@ -2,6 +2,9 @@ package view.listeners;
 
 import scheduleModel.ISchedule;
 
+/* Makes sure that every class that implements this interface
+ * has appropriate implementations for when branches, timer or schedule
+  * needs to be updated. */
 public interface AlgorithmListener {
     void bestScheduleUpdated(ISchedule schedule);
     void algorithmFinished();
