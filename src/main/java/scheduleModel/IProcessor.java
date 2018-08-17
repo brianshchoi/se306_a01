1fffd4d373e022035a754c90107a419fbe4ec1dc
Zain's University Laptop <zain.zain51@gmail.com>
2018-08-17 20:14:31 +1200

    Finished commenting schedule model

--- src/main/java/scheduleModel/IProcessor.java
+++ src/main/java/scheduleModel/IProcessor.java
@@ -9,7 +9,7 @@ public interface IProcessor {
     /**
      * Checks if task is scheduled to this processor
      * @param task
-     * @return
+     * @return true if the task is scheduled
      */
     boolean contains(Task task);
 
@@ -21,17 +21,22 @@ public interface IProcessor {
 
     /**
      * Returns finish time of last task on this processor
-     * @return
+     * @return the finishing time
      */
     int getFinishTime();
 
     /**
      * Returns finish time of task on this processor
      * @param task
-     * @return
+     * @return the finishing time of that task
      */
     int getFinishTimeOf(Task task);
 
+    /**
+     * Returns finish time of task on this processor
+     * @param task
+     * @return the start time of that task
+     */
     int getStartTimeOf(Task task);
 
     /**
@@ -43,13 +48,28 @@ public interface IProcessor {
 
     /**
      * Returns all tasks that have been scheduled on the processor
-     * @return
+     * @return a List of tasks
      */
     List<Task> getTasks();
 
+    /**
+     * Get the ID of this processor.
+     * @return an integer ID
+     */
     int getId();
 
+    /**
+     * Get the free time between tasks on this processor.
+     * @return an integer representing the idle time
+     */
     int getIdleTime();
 
+    /**
+     * Check if this processor is equivalent to another processor.
+     * Equivalent means same tasks scheduled at the same times
+     * (does not have to be the same processor number).
+     * @param processor
+     * @return true if the processor is equivalent
+     */
     boolean isEquivalent(Processor processor);
 }
--- src/main/java/scheduleModel/IScheduler.java
+++ src/main/java/scheduleModel/IScheduler.java
@@ -3,11 +3,29 @@ package scheduleModel;
 import taskModel.Task;
 
 public interface IScheduler {
-
-    // Removes a particular task from a schedule
+    /**
+     * Removes a particular task from a schedule
+     * @param task
+     * @param schedule
+     */
     void remove(Task task, ISchedule schedule);
 
-    void schedule(Task task, IProcessor iProcessor, ISchedule schedule);
+    /**
+     * Given a task, a schedule, and a particular processor,
+     * schedule that task on that processor on the schedule.
+     * @param task
+     * @param processor
+     * @param schedule
+     */
+    void schedule(Task task, IProcessor processor, ISchedule schedule);
 
+    /**
+     * Get the earliest starting time of a given task on a given processor
+     * for a given schedule.
+     * @param task
+     * @param processor
+     * @param schedule
+     * @return
+     */
     int getEarliestStartTime(Task task, IProcessor processor, ISchedule schedule);
 }
