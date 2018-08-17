e034762f3b75cb69bfd4e7e8a4996eb41ccff838
Zain's University Laptop <zain.zain51@gmail.com>
2018-08-17 19:39:53 +1200

    removed underscores from field names

--- src/main/java/fileIO/DotGraph.java
+++ src/main/java/fileIO/DotGraph.java
@@ -9,24 +9,22 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class DotGraph {
 
     private static final String OUTPUT_PREFIX = "output";
-    private String _title;
-    private String _filename;
-    private ISchedule _schedule;
-    private TaskModel _taskModel;
+    private String title;
+    private String filename;
+    private ISchedule schedule;
+    private TaskModel taskModel;
 
     public DotGraph(String filename, String title, ISchedule schedule, TaskModel taskModel){
         // Capitalise first letter of graph name and add "output" prefix
-        this._title = OUTPUT_PREFIX + title.substring(0, 1).toUpperCase() + title.substring(1);
-        this._schedule = schedule;
-        this._taskModel = taskModel;
-        this._filename = filename;
+        this.title = OUTPUT_PREFIX + title.substring(0, 1).toUpperCase() + title.substring(1);
+        this.schedule = schedule;
+        this.taskModel = taskModel;
+        this.filename = filename;
     }
 
     // This method generates the optimal schedule digraph in DOT syntax
@@ -34,14 +32,14 @@ public class DotGraph {
         final StringBuilder output = new StringBuilder();
 
         // Append title of digraph and opening brace
-        output.append(DotRenderer.openGraph(_title));
+        output.append(DotRenderer.openGraph(title));
 
         // Iterate through all tasks
-        List<Task> tasks = _taskModel.getTasks();
+        List<Task> tasks = taskModel.getTasks();
         DotRenderer.sortTasks(tasks);
         for (Task task: tasks){
-            int startTime = _schedule.getFinishTimeOf(task) - task.getWeight();
-            IProcessor processor = _schedule.getProcessorOf(task);
+            int startTime = schedule.getFinishTimeOf(task) - task.getWeight();
+            IProcessor processor = schedule.getProcessorOf(task);
 
             // Add all task
             output.append(DotRenderer.addNode(task, startTime, processor));
@@ -67,7 +65,7 @@ public class DotGraph {
 
     // This method takes the graph string and outputs a dot graph with the filename
     private void writeDotFile(String graph){
-        try (PrintStream out = new PrintStream(new FileOutputStream(_filename))) {
+        try (PrintStream out = new PrintStream(new FileOutputStream(filename))) {
             out.print(graph);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
